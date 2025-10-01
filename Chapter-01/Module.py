23e4d4458768a830db6c5efd2df47486e86b00ac
EzVedant07 <patelvedant0914@gmail.com>
2025-10-01 21:47:24 +0530

    Add files via upload

---
+++ Chapter-01/Module.py
@@ -0,0 +1,11 @@
+# pyjokes module is used to fetch programming jokes
+
+import pyjokes
+
+# Get a random programming joke
+joke = pyjokes.get_joke()
+
+print("😂 Here's a joke for you:")
+print(joke)
+# You can run this code after installing the pyjokes module using pip:
+# pip install pyjokes
