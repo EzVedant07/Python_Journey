6403ea6adaca2e33a7eef1111e0891d4ce38a9e4
EzVedant07 <patelvedant0914@gmail.com>
2025-10-01 21:32:57 +0530

    Add files via upload

---
+++ Chapter-01/Hello.py
@@ -0,0 +1,18 @@
+# DAY 01: DATE 01/10/2025
+# This is a simple Python program that prints "Hello, World!" to the console.
+
+print("Hello, World!")
+
+# Printing a poem using multi-line string
+print('''
+    Twinkle, twinkle, little star, how I wonder what you are. Up above the world so high,
+like a diamond in the sky. Twinkle, twinkle, little star, how I wonder what you are.
+When the blazing sun is set, and the grass with dew is wet. Then you show your little
+light, twinkle, twinkle all the night. Twinkle, twinkle little star, how I wonder what you
+are.
+Then the traveler in the dark thanks you for your tiny spark. How could he see where to
+go if you did not twinkle so? Twinkle, twinkle little star, how I wonder what you are.
+As your bright and tiny spark lights the traveler in the dark, though I know not what you
+are, twinkle, twinkle, little star. Twinkle, twinkle, little star, how I wonder what you are.
+''')
+# End of the program
